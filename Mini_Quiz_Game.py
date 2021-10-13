104b97e95892029f4aa935061a15ce0ffd524472
comprehensivegene649837 <92254249+comprehensivegene649837@users.noreply.github.com>
2021-10-14 01:05:19 +0530

    Update Mini_Quiz_Game.py

--- Mini_Quiz_Game.py
+++ Mini_Quiz_Game.py
@@ -1,38 +1,3 @@
-#class QuizGame:
-#    
-#    def __init__(self, s, r):
-#        self.s= s
-#        self.r= r
-#    
-#    def getInfo(self):
-#        if self.s == "yes":
-#            return "hurray! you got one point"
-#    
-#        elif self.s== "no":
-#            return "sorry! you lost"
-#    
-#        else: #self.s!= "yes" or self.r!= "no":
-#            return "invalid input!"
-#
-#   
-#
-#    def rectangle(self):
-#        if self.r== ("length*breadth") or self.r==("breadth*length"):
-#                return "hurray! you got one point"
-#
-#        else:  #self.r!= ("length*breadth") or self.r!=("breadth*length"):
-#            return"invalid input!"
-#    
-#
-#
-#
-#
-#o= input("is the Earth spherical in shape:  ")
-#j= input("What is the area of rectangle: ")
-#a= QuizGame(o, j)
-#print(a.getInfo())
-#print(a.rectangle())
-
 import random
 
 print("Is the earth spherical in shape: ")
@@ -88,8 +53,6 @@ def quizGame2(a): #to make the second question
 
 print(quizGame(input("enter your answer: "))) #input the first answer 
 print(quizGame2(input("what is the area of rectangle: "))) #input the second answer
-#def quizGame()
-#def quizGame2()
 print(f' total points = : {points}')
 
 
